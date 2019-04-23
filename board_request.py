c078a013f5728ba46a86a8069089eddb7f7dd152
Jia Li <jia3@g.clemson.edu>
2019-04-23 19:29:42 -0400

    write html to a folder named output

--- board_request.py
+++ board_request.py
@@ -1,8 +1,24 @@
-import urllib.request
+'''import urllib.request
 	
 response = urllib.request.urlopen('https://boardgamegeek.com/browse/boardgame')
 	
 html = response.read()
 	
-print(html)
+print(html)'''
 	
+import urllib.request
+import os
+	
+response = urllib.request.urlopen('https://boardgamegeek.com/browse/boardgame')
+	
+html = response.read()
+	
+#print(html)
+	
+	
+if not os.path.exists("output"):
+	os.mkdir("output")
+	
+f = open("output/boardgamegeek.html", "wb")
+
+f.write(html)
