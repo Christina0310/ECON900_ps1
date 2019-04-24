d7b188e30175a51750d040295d84d4f380f1e03b
Jia Li <jia3@g.clemson.edu>
2019-04-23 21:38:59 -0400

    parse 160 files and write to csv

--- board_parse.py
+++ board_parse.py
@@ -8,38 +8,39 @@ if not os.path.exists("parsed_files"):
 
 df = pd.DataFrame()
 
+for i in range(1,50):#one_file_name in glob.glob("html_files/*.html"):
+        current_page = str(i)
+        one_file_name ="html_files/boardgamegeek" + current_page + ".html"
+        print("parsing " + one_file_name)
+        f = open(one_file_name, "r")
+        soup = BeautifulSoup(f.read(), 'html.parser')
+        f.close()
+        boardgames_table = soup.find("table", {"id": "collectionitems"})
+        boardgames_rows = boardgames_table.find_all("tr", {"id":"row_"})
+        for r in boardgames_rows: 
+            boardgames_name = r.find("td", {"class": "collection_objectname"}).find("div",{"style":"z-index:1000;"}).find("a").text
+            boardgames_neg_year = r.find("td", {"class": "collection_objectname"}).find("div",{"style":"z-index:1000;"}).find("a").text[1]
+            boardgames_ratings= r.find_all("td",{"class":"collection_bggrating"})
+            boardgames_geek_ratingq= boardgames_ratings[0].text #next(game_iter).text
+            boardgames_ave_ratingq = boardgames_ratings[1].text
+            boardgames_num_votersq = boardgames_ratings[2].text
+            boardgames_year = boardgames_neg_year[1:5]
+            boardgames_geek_rating = float(boardgames_geek_ratingq)
+            boardgames_ave_rating = float(boardgames_ave_ratingq)
+            boardgames_num_voters = int(boardgames_num_votersq)
+            df = df.append({ 
+                'title': boardgames_name,
+                'year': boardgames_year,
+                'geek_rating': boardgames_geek_rating,
+                'average_rating': boardgames_ave_rating,
+                'num_of_voters': boardgames_num_voters
+                }, ignore_index=True)
 
 
-f = open("html_files/boardgamegeek1.html", "r")
 
-#print(f.read())
-
-
-soup = BeautifulSoup(f.read(), 'html.parser')
-
-boardgames_table = soup.find("table", {"id": "collectionitems"})
-#boardgames_tbody = boardgames_table.find("tbody")
-boardgames_rows = boardgames_table.find_all("tr", {"id":"row_"})
+df.to_csv("parsed_files/boardgeekgame_dataset.csv")
 
 
 
-boardgames_name = boardgames_rows[0].find("td", {"class": "collection_objectname"}).find("div",{"id":"results_objectname1"}).find("a").text
-boardgames_ratings= boardgames_rows[0].find_all("td",{"class":"collection_bggrating"})
-#game_iter = iter(boardgames_ratings)
-boardgames_geek_rating= boardgames_ratings[0].text #next(game_iter).text
-boardgames_ave_rating = boardgames_ratings[1].text
-boardgames_num_voters = boardgames_ratings[2].text
-#boardgames_rating2 = next(game_iter).text
-
-#boardgames_geekrating = boardgames_ratings[0]#.find("td", {"class":"collection_bggrating"})
 
 
-print(boardgames_name)
-print(boardgames_geek_rating)
-print(boardgames_ave_rating)
-print(boardgames_num_voters)
-#print(boardgames_price)
-
-'''for r in boardgames_rows: 
-    boardgames_name = r.find("td", {"class": "collection_objectname"}).find("div",{"style":"z-index:1000;"}).find("a").text
-    boardgames_rating'''
